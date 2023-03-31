74f198f866841e5a8c576fa94472eb9bc1712da0
msi <shakirov.vitaliy@gmail.com>
2023-03-31 10:55:24 +0300

    Add requirements

--- tutorTellcap/settings.py
+++ tutorTellcap/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'tutorTellcap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': config('DATABASE_NAME'),
+            'HOST': config('DATABASE_HOST'),
+            'USER': config('DATABASE_USERNAME'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
