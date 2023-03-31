49256d25bb08fccf811ffd870c1034accd7d45ec
msi <shakirov.vitaliy@gmail.com>
2023-03-31 12:28:07 +0300

    Add requirements

--- tutorTellcap/settings.py
+++ tutorTellcap/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['localhost', config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['192.168.10.108', 'tutor.tellcap.ru', 'localhost', '127.0.0.1']
 
 # Application definition
 
